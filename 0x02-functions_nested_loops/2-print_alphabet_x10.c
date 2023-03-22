a3dc23cb7e822b65ea75f3d66f39904bdffea990
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-03-22 05:37:48 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int i, ch;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (ch = 'a' ; ch <= 'z' ; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		putchar('\n');
 	}
