4bad637fe13f877ef8b7fd396b3e2278dee4577d
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-04-09 19:12:33 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,10 @@
  * Return: 0
  */
 
-int main(int argc, int char **argv)
+int main(int argc, char **argv)
 {
-	printf("%d`n", argc - 1);
+	(void)argv;
+
+	printf("%d\n", argc - 1);
 	return (0);
 }
