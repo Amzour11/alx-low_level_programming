2546ff9556c6f420dd83364f4a787e13294f466b
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-04-17 15:12:25 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,7 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (k = 0; i < i; k++)
+	for (k = 0; k < i; k++)
 		dog->name[k] = name[k];
 	dog->age = age;
 	for (j = 0; owner[j]; j++)
