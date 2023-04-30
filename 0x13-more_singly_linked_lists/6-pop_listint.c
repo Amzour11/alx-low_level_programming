d1cc13354e025e5cc8aab176d030d2ef30d32ae2
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-04-30 11:43:25 -0700

    singly linked lists commits

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,22 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list
- * @head: the address of pointer to the first node
+ *pop_listint - deletes the head node of a listint_t
+ * @head: address of pointer to first node
  *
  * Return: popped node's value
  */
-
 int pop_listint(listint_t **head)
-{
+{#
 	listint_t *node;
-	int i;
+	int n;
 
 	if (!head || !*head)
 		return (0);
 
 	node = (*head)->next;
-	i = (*head)->i;
+	n = (*head)->n;
 	free(*head);
 	*head = node;
-	return (i);
+	return (n);
 }
