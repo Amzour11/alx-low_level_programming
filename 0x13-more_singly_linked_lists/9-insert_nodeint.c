de65868e3225c07b022eb8063d04ffe4bd278d05
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-05-01 11:32:34 -0700

    more singly linked lists commits

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *new_node = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+	unsigned int n = 0;
 
 	if (!head || !new_node)
 		return (NULL);
@@ -28,13 +28,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = *head;
 	while (node)
 	{
-		if (i == index - 1)
+		if (n == index - 1)
 		{
 			new_node->next = node->next;
 			node->next = new_node;
 			return (new_node);
 		}
-		i++;
+		n++;
 		node = node->next;
 	}
 	free(new_node);
