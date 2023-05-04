61a7075660c788659c2643e2b2e62ad57d188061
Mohamed Amzour <mohamedamzour11@gmail.com>
2023-05-04 11:41:53 -0700

    bit manipulation commits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip to get from one number to another.
+ * @n: the first number
+ * @m: the second number
+ *
+ * Return: the number of flipped bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int difference, result;
+	unsigned int a, b;
+
+	b = 0;
+	result = 1;
+	differnce = n ^ m;
+	for (a = 0; a < (sizeof(unsigned long int) * 8); a++)
+	{
+		if (result == (differnce & result))
+			b++;
+		result <<= 1;
+	}
+
+	return (b);
+}
